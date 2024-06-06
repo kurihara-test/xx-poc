d593ddc32053c20fc76dc165bd2e08ea3cbedc84
kuritify <kuri.creep.kuri@gmail.com>
2024-06-06 15:10:39 +0900

    fix: index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-// comment
-console.log(hello world);
-// fugafuga fugafuga
-console.log(hello world);
-console.log(hello world);
-console.log(hello world);
-console.log(hello world);
+console.log('hello world');
